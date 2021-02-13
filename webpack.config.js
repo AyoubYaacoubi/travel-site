1db4a90d9368b08642297d8a7e148c7dcdcaf4aa
unknown <AyoubYaacoubi16@gmail.com>
2021-02-13 11:12:36 +0100

    starting the app

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const postCSSPlugins = [
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './App/assets/scripts/App.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {test: /\.css$/i, use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions:{plusgins: postCSSPlugins}}}]}
+        ]
+    }
+}
