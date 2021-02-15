211c60cf455c98da3d3a1b908d26cc8eca843b09
unknown <AyoubYaacoubi16@gmail.com>
2021-02-15 12:19:00 +0100

    more plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
     require('postcss-import'),
     require('autoprefixer')
 ]
