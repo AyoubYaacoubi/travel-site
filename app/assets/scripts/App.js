06895e0a9f02f74f366a798419d3e40c3caf328c
unknown <AyoubYaacoubi16@gmail.com>
2021-03-02 17:54:03 +0100

    cool animations with a good regard to preformance

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/style.css'
 import MobileNav from './modules/_MobileNav'
+import RevealOnScroll from './modules/_RevealOnScroll'
 
 let mobileNav = new MobileNav()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 if(module.hot){
     module.hot.accept()
---
+++ app/assets/scripts/modules/_RevealOnScroll.js
@@ -0,0 +1,50 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class RevealOnScroll{
+    constructor(els, tresholdPercent){
+        this.itemsToReveal = els
+        this.tresholdPercent = tresholdPercent
+        this.hideInitially()
+        this.windowHeight = window.innerHeight
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+        this.event()
+    }
+    event(){
+        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("resize", debounce(()=>{
+            this.windowHeight = window.innerHeight
+        }, 335))
+    }
+
+    calcCaller(){
+        this.itemsToReveal.forEach(el=>{
+            if(el.isRevealed == false){
+                this.calculateOnScroll(el)
+            }
+        })
+    }
+
+    calculateOnScroll(el){
+        if(window.scrollY + this.windowHeight > el.offsetTop){
+            let scrollPercent = (el.getBoundingClientRect().y / this.windowHeight) * 100
+            if(scrollPercent < this.tresholdPercent){
+                el.classList.add("reveal-item--is-visible")
+                el.isRevealed = true
+                if(el.isLastItem){
+                    window.removeEventListener("scroll", this.scrollThrottle)
+                }
+            }
+        }
+    }
+
+    hideInitially(){
+        this.itemsToReveal.forEach(el=>{
+            el.classList.add('reveal-item')
+            el.isRevealed = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+    }
+}
+
+export default RevealOnScroll
