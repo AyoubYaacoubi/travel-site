77ce26eaa121ce491c1cb386b3303abef4959ce0
unknown <AyoubYaacoubi16@gmail.com>
2021-03-05 14:13:19 +0100

    active state for the links

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import '../styles/style.css'
 import MobileNav from './modules/_MobileNav'
 import RevealOnScroll from './modules/_RevealOnScroll'
+import StickyHeader from './modules/_StickyHeader'
 
 let mobileNav = new MobileNav()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new StickyHeader()
 
 if(module.hot){
     module.hot.accept()
