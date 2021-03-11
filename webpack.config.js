6a8887e7987fd8c1d42994e50e95976988fe9a3d
unknown <AyoubYaacoubi16@gmail.com>
2021-03-11 20:40:33 +0100

    spliting our modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
-const path = require('path')
+let currentTask = process.env.npm_lifecycle_event
+const path = require("path")
 
-const postCSSPlugins = [
-    require('postcss-hexrgba'),
-    require("postcss-simple-vars"),
-    require('postcss-mixins'),
-    require("postcss-nested"),
-    require("postcss-import"),
-    require("autoprefixer")
-]
+const postCSSPlugins = [require("postcss-hexrgba"), require("postcss-simple-vars"), require("postcss-mixins"), require("postcss-nested"), require("postcss-import"), require("autoprefixer")]
 
-module.exports = {
-    entry: './App/assets/scripts/App.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, './app')
-    },
-// to recompile the bundle: http://localhost:3000/invalidate
-    devServer: {
-        before: function(app, server){
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.resolve(__dirname, './app'),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
-    // watch: true, // if we do not use the dev server
-    module: {
-        rules: [
-            {test: /\.css$/i, use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]}
-        ]
-    }
+let config = {
+  entry: "./App/assets/scripts/App.js",
+  module: {
+    rules: [{test: /\.css$/i, use: ["style-loader", "css-loader?url=false", {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]}],
+  },
 }
+
+if (currentTask == "dev") {
+  config.output = {
+    filename: "main.js",
+    path: path.resolve(__dirname, "app"),
+  }
+  config.devServer = {
+    before: function (app, server) {
+      server._watch("./app/**/*.html")
+    },
+    contentBase: path.resolve(__dirname, "./app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  }
+  config.mode = "development"
+}
+
+if (currentTask == "build") {
+  config.output = {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  }
+  config.mode = "production"
+}
+
+module.exports = config
+
+// dont forgot the package task initializer: ===========
