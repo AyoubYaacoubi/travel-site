c7f935a943d18dcbf6d32ee2dd6028c2598d0c53
unknown <AyoubYaacoubi16@gmail.com>
2021-03-15 20:17:42 +0100

    finaly close to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,58 @@
 let currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const fse = require("fs-extra")
 
-const postCSSPlugins = [require("postcss-hexrgba"), require("postcss-simple-vars"), require("postcss-mixins"), require("postcss-nested"), require("postcss-import"), require("autoprefixer")]
+// const postCSSPlugins = [require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-mixins"), require("autoprefixer")]
+const postCSSPlugins = ["postcss-import", "postcss-simple-vars", "postcss-nested", "postcss-mixins", "autoprefixer"]
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    "css-loader?url=false",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {plugins: postCSSPlugins},
+      },
+    },
+  ],
+}
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html")
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
+    })
+  })
 
 let config = {
   entry: "./App/assets/scripts/App.js",
+
   module: {
-    rules: [{test: /\.css$/i, use: ["style-loader", "css-loader?url=false", {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]}],
+    rules: [cssConfig],
   },
+
+  plugins: pages,
 }
 
+// the development proccess here:
 if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
+
   config.output = {
     filename: "main.js",
     path: path.resolve(__dirname, "app"),
   }
+
   config.devServer = {
     before: function (app, server) {
       server._watch("./app/**/*.html")
@@ -24,15 +62,29 @@ if (currentTask == "dev") {
     port: 3000,
     host: "0.0.0.0",
   }
+
   config.mode = "development"
 }
 
+// the build proccess here:
 if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+
   config.output = {
-    filename: "main.js",
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "dist"),
   }
+
   config.mode = "production"
+
+  config.optimization = {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+    splitChunks: {chunks: "all"},
+  }
+
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: "style.[chunkhash].css"}))
 }
 
 module.exports = config
