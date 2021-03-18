80da0ae1d1f4541c6b0c3cbefe2ddb05b21cc0e6
unknown <AyoubYaacoubi16@gmail.com>
2021-03-18 19:03:11 +0100

    hellio

--- aws/aws.js
+++ aws/aws.js
@@ -1,7 +1,7 @@
 exports.handler = function (event, context, callback) {
   let secretContent = `
-    <h1 style="color: lightblue; text-transform: uppercase; text-align:center;">welcom to the inner circle!</h>
-    <p style="font-style: italic;">in this little seminar you'll learn how to make 100k  a day, new previous knowledge required. just like <strong>butter</strong></p>
+    <h1 style="color: lightblue; text-transform: uppercase; text-align:center;">welcom to the inner circle!</h1>
+    <p style="font-style: italic;">in this little seminar you'll learn how to make 100k  a day, no previous knowledge required. just like <strong>butter</strong></p>
   `
   let body
   if (event.body) {
